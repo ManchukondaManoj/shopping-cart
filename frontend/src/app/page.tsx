4ff21aaae4b329c436420ab5ce16a452326957bd
Manoj <manchukondamanoj@gmail.com>
2025-03-05 23:05:04 +0530

    Fix UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -53,7 +53,7 @@ const HomeScreen = () => {
         />
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {products.map((product) => (
-            <Product key={product._id} product={product} />
+            <Product key={product.productId} product={product} />
           ))}
         </div>
       </div>
