182d225fc36a67e4490c616d02025bf75f2666dc
Manoj Kumar <manchukondamanoj@gmail.com>
2025-03-11 11:46:53 +0530

    Update serviceAccountDetails.js

--- functions/backend/serviceAccountDetails.js
+++ functions/backend/serviceAccountDetails.js
@@ -1,18 +1,5 @@
 module.exports = () => {
   return {
-    type: "service_account",
-    project_id: "shoppingcart-app-b2a2b",
-    private_key_id: "dd65d6dfbac3249821ab06581ff6165e40f47280",
-    private_key:
-      "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCzADFoKx0Uws37\n0AcdtUv0/wZZK7s/YBkuDGsqagLHmIv8sMpCY8MVAGXdRnx0qB3NEFNGWXmKtQmW\n5/9dKnoHD7J4xKQvmZKymM6xPQhW/WS7f5qsMBOf7LAFq7ym9WLbGMkXuLN1uQKz\nX1lvd0QWXVKumhna+F4x4ZJiMMaTL6DcgslUUA5SaO8l3q4Tv77DyMFTOC4QUccV\nSWBTCOrRBMj/LDcro+duTR7a+ziCJQCtCdoPWIVx8Nl1td/ta/2WDd7qYxKnBPnC\nr8TNBwPYKm686SBbeFV6XLIrDVJr20Vitsm8gYPHJc1HT/u2THjC2fFjcTtLa/I5\nl+pWzkYXAgMBAAECggEAHPLwCp/GYUAsHgiDzhsvAJ1qAmvfKZ26FMdvYXmpn2wd\nX62b2GbX+wKIduPaZud1gl+ciCkaTgN/XIeCtieTeqbgP0XWI9ibcATkkKeFPMRY\nUTaO5kAUPG4D/bbLmNjgtvxoMUzuwSA9vpKyHSPVwCQxpglIGzDF0x1fFkA7LTxm\nYAXWAXgicUGQLSChWhiN1o5/l0GO82m0tL5Ya5b9uc/AFWT6JsukiRYYg7f2Lirq\nOZszCyNiNjM1Tsa/jg8DeVgLLaRpe2VB52Zc8Id6cfJmS9s6dEV0NiQlU67nPpSq\nVQvJBvcJ47PvJiE5EVFsEoswMShR52LLaf6p2Rh9SQKBgQDgMrNn4IxTsYFG3TIo\neRs2Av3g1oFob/ycwhYv1kJiJC6ktHQ1k7hfihar6p2zqpv42ovoV47Q0hoE58Rn\n0tSIImBoB40OsuuY0JVbtjOTjwjVYauDyQK++1asgTJAAI5ha29i3dHiCjbdsZiM\nHoqBe4hLqhEXPYM0VGaRh1xs/QKBgQDMZD5jYIkDwUtTx3ul+dtjQ749v6m/lrQu\nZWLW+iUcO2CXL2yWMIZq3abkUZIN+xYl+h397mmE2/6ljXukSOdecwl/7U3ih7St\nOtiiEbgxxFEBheggOCDlH+x06ti06QtRDIG656tiY+7XxKlTXmf2KxoHZDLsrZfn\n4PCevV61owKBgAzEVlsP+m25qfbzGawcWWRV/0cScAQWfHggxUqsFNOp484VSJjU\ndKdcIbZggeJ9UKx3JYhxk/xoOnsVB6Yx51BpRLwn0eyIW8oz9o1mbsF0X7hm+3Fh\nvPpLLcBdCxOB9JmvmIQ0xYLtufod5uXzx0dgVX6ZjKeaJSKRu9WFs8mxAoGAVO9r\nRZy+E0Nl7CC/b0+PLYbfLYXSIn2bzpPvKOq2NS8vimP4ucjcctg9HDARg6HNqGZx\nxC4oxz8saNuMf86T5xni3rhcX0CJglsr3t2CL5wnFXzx40Gv2ccUWdmlyDf3reyG\nlIblH3PrstpUiwJ2LUPoAx5OUzAoKgG9vBlxnAUCgYEAtQppCG21PyekWcUJlljx\njcOFK1bxJoiOI9BeCpjstTbd8DU9auJguqMzMDRo8xBnJB0NncsQJYe0jbnjni9y\nVuyeQ35JfhqqMqoWjYy2PcURZU1++ncRt5qZrp5FGy073yR0qBiHsjUXsqziSxlJ\nwQOviUKoQSeXREm3cz9GZ9c=\n-----END PRIVATE KEY-----\n",
-    client_email:
-      "firebase-adminsdk-fbsvc@shoppingcart-app-b2a2b.iam.gserviceaccount.com",
-    client_id: "103859531910170972656",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40shoppingcart-app-b2a2b.iam.gserviceaccount.com",
-    universe_domain: "googleapis.com",
+
   };
 };
